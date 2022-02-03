29bc157c074fd9e93a5766067ebd6dd54dd2a0d6
Chris Kaltwasser <chris@chrisk.com>
2022-02-03 17:13:32 -0500

    added spawn test

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,12 @@ fn main() {
     let stdout = File::create("/tmp/daemon.out").unwrap();
     let stderr = File::create("/tmp/daemon.err").unwrap();
 
-
     let daemonize = Daemonize::new()
         .pid_file("/tmp/test.pid") // Every method except `new` and `start`
         .working_directory("/tmp") // for default behaviour.
         .stdout(stdout)  // Redirect stdout to `/tmp/daemon.out`.
         .stderr(stderr)  // Redirect stderr to `/tmp/daemon.err`.
         .privileged_action(|| "Executed before drop privileges");
-
 
     match daemonize.start() {
         Ok(_) => {
@@ -45,5 +43,6 @@ fn main() {
 }
 
 async fn handler() -> Html<&'static str> {
+    assert_eq!(tokio::spawn(async { 1 }).await.unwrap(), 1);
     Html("<h1>Hello, World!</h1>")
 }
